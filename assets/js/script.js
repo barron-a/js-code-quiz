f489761b251197d2ad7b733aee562a646b075639
Adam Barron <adambarron@me.com>
2020-10-11 12:28:14 -0700

    add quizTimer function and ability to begin with 'Start Quiz' button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
 // establish variables
+var startButton = document.getElementById('start-quiz');
+
+function quizTimer() {
+    var timeLeft = 5;
+    var interval = setInterval(function() {
+        timeLeft--;
+        document.getElementById("timer").textContent = "Time: " + timeLeft;
+        if (timeLeft <= 0) {
+            clearInterval(interval);
+            return;
+        }
+    },
+    1000);
+}
+
+startButton.addEventListener("click", quizTimer);
