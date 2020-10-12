aae01c78d20c44a9dac4d6b5575c5603a5cf2b58
Adam Barron <adambarron@me.com>
2020-10-11 22:37:37 -0700

    added for loop to display questions and possible answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,26 +2,16 @@
 var startButton = document.getElementById('start-quiz');
 var quizQuestions = [
     { 
-        question: "Which of the following examples would properly link a JavaScript document to an HTML document?", 
-        answers: {
-            1: "<link rel=\"stylesheet\" href=\"style.css\">",
-            2: "<script> src=\"./assets/js/script.js\"</script>",
-            3: "<script src=\"./assets/js/script.js\"></script>",
-            4: "<link rel=\"script\" href=\"script.js\">",
-        },
-        rightAnswer: "3" 
+        question: "Which of the following is used in the script tag to link a JavaScript file to an HTML file?", 
+        answers: ["img", "rel", "src", "type"],
+        rightAnswer: "1" 
     },
     { 
         question: "__________ are containers that store values", 
-        answers: {
-            1: "objects",
-            2: "variables",
-            3: "functions",
-            4: "for loops",
-        },
+        answers: ["objects", "variables", "functions", "for loops"],
         rightAnswer: "2" 
     }
-]
+];
 
 function quizTimer() {
     var timeLeft = 90;
@@ -34,13 +24,47 @@ function quizTimer() {
         }
     },
     1000);
-}
+};
+
+function removeintro() {
+    var removeTitle = document.getElementById("title");
+    var removeIntroText = document.getElementById("intro-text");
+    var removeStartBtn = document.getElementById("start-quiz");
+    removeTitle.remove();
+    removeIntroText.remove();
+    removeStartBtn.remove();
+};
 
 function quiz() {
     for (var i = 0; i < quizQuestions.length; i++) {
+        var currentQuestion = document.createElement("h2");
+        var optionOne = document.createElement("button");
+        var optionTwo = document.createElement("button");
+        var optionThree = document.createElement("button");
+        var optionFour = document.createElement("button");
+        var question = quizQuestions[i].question;
+        var answers = quizQuestions[i].answers;
+        var rightAnswer = quizQuestions[i].rightAnswer;
 
+        //display current question from array on page
+        currentQuestion.innerHTML = question;
+        document.getElementById("main").appendChild(currentQuestion);
+
+        //display current possible answers from array on page
+        optionOne.innerHTML = answers[0];
+        document.getElementById("main").appendChild(optionOne);
+
+        optionTwo.innerHTML = answers[1];
+        document.getElementById("main").appendChild(optionTwo);
+
+        optionThree.innerHTML = answers[2];
+        document.getElementById("main").appendChild(optionThree);
+
+        optionFour.innerHTML = answers[3];
+        document.getElementById("main").appendChild(optionFour);
     }
-}
+};
 
 startButton.addEventListener("click", quizTimer);
 startButton.addEventListener("click", quiz);
+startButton.addEventListener("click", removeintro);
