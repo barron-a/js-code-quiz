f82618f7fa8696e2dfa2ca89a903781475060692
Adam Barron <adambarron@me.com>
2020-10-24 11:55:20 -0700

    remove commented out and broken code for ease of reading

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,18 +33,9 @@ function startQuiz() {
     intro.setAttribute("class", "hidden");
     countdown = setInterval(quizTimer, 1000);
     timer.textContent = "Time: " + timeLeft;
-    // var removeTitle = document.getElementById("title");
-    // var removeIntroText = document.getElementById("intro-text");
-    // var removeStartBtn = document.getElementById("start-quiz");
-    // removeTitle.remove();
-    // removeIntroText.remove();
-    // removeStartBtn.remove();
 };
 
 function displayQuestion() {
-
-    // var currentQuestion = document.createElement("h2");
-    // currentQuestion.setAttribute("id", "question");
 
     questionContainer.removeAttribute("class");
 
@@ -67,45 +58,6 @@ function displayQuestion() {
     var optionFour = document.getElementById("three");
     optionFour.textContent = currentQuestion.answers[3];
     optionFour.addEventListener("click", checkAnswer);
-
-    // var optionOne = document.createElement("button");
-    // optionOne.setAttribute("class", "answerButton");
-    // optionOne.setAttribute("id", "zero");
-    // optionOne.addEventListener("click", checkAnswer);
-
-    // var optionTwo = document.createElement("button");
-    // optionTwo.setAttribute("class", "answerButton");
-    // optionTwo.setAttribute("id", "one");
-    // optionTwo.addEventListener("click", checkAnswer);
-
-    // var optionThree = document.createElement("button");
-    // optionThree.setAttribute("class", "answerButton");
-    // optionThree.setAttribute("id", "two");
-    // optionThree.addEventListener("click", checkAnswer);
-
-    // var optionFour = document.createElement("button");
-    // optionFour.setAttribute("class", "answerButton");
-    // optionFour.setAttribute("id", "three");
-    // optionFour.addEventListener("click", checkAnswer);
-
-    //display current question from array on page
-    // currentQuestion.innerHTML = question;
-    // document.getElementById("question-container").appendChild(currentQuestion);
-
-    // answerContainer.innerHTML = "";
-
-    //display current possible answers from array
-    // optionOne.innerHTML = quizQuestions[index].answers[0];
-    // document.getElementById("button-container-one").appendChild(optionOne);
-
-    // optionTwo.innerHTML = quizQuestions[index].answers[1];
-    // document.getElementById("button-container-two").appendChild(optionTwo);
-
-    // optionThree.innerHTML = quizQuestions[index].answers[2];
-    // document.getElementById("button-container-three").appendChild(optionThree);
-
-    // optionFour.innerHTML = quizQuestions[index].answers[3];
-    // document.getElementById("button-container-four").appendChild(optionFour);
 }
 
 function endQuiz() {
@@ -174,33 +126,3 @@ startButton.addEventListener("click", quizTimer);
 startButton.addEventListener("click", displayQuestion);
 startButton.addEventListener("click", startQuiz);
 submitScore.addEventListener("click", saveScore);
-
-// function quiz() {
-//     for (var i = 0; i < quizQuestions.length; i++) {
-//         var currentQuestion = document.createElement("h2");
-//         var optionOne = document.createElement("button");
-//         var optionTwo = document.createElement("button");
-//         var optionThree = document.createElement("button");
-//         var optionFour = document.createElement("button");
-//         var question = quizQuestions[i].question;
-//         var answers = quizQuestions[i].answers;
-//         var rightAnswer = quizQuestions[i].rightAnswer;
-
-//         //display current question from array on page
-//         currentQuestion.innerHTML = question;
-//         document.getElementById("main").appendChild(currentQuestion);
-
-//         //display current possible answers from array on page
-//         optionOne.innerHTML = answers[0];
-//         document.getElementById("main").appendChild(optionOne);
-
-//         optionTwo.innerHTML = answers[1];
-//         document.getElementById("main").appendChild(optionTwo);
-
-//         optionThree.innerHTML = answers[2];
-//         document.getElementById("main").appendChild(optionThree);
-
-//         optionFour.innerHTML = answers[3];
-//         document.getElementById("main").appendChild(optionFour);
-//     }
-// };
