32985458504da91dc6733957ce0ba03ab344cd84
Adam Barron <adambarron@me.com>
2020-10-24 22:52:17 -0700

    added showScores function and clearScores function to highscores.js

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,3 +1,22 @@
+var clearLocalStorage = document.getElementById("clear-scores");
+
 function showScores() {
+    var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
     
+    for (i = 0; i < highScores.length; i++) {
+        var scoreListItem = document.createElement("li");
+        scoreListItem.textContent = highScores[i].initials + ": " + highScores[i].score;
+        console.log(highScores[i].initials);
+        var scoreList = document.getElementById("high-scores");
+        scoreList.appendChild(scoreListItem);
+    };
 }
+
+function clearScores() {
+    localStorage.clear();
+    location.reload();
+}
+
+showScores();
+
+clearLocalStorage.addEventListener("click", clearScores);
